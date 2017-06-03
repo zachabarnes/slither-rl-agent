7addcab271f230764c141116c33dcf797cf99ced
Zachary Barnes <zach.a.barnes@gmail.com>
2017-06-03 12:13:43 -0700

    plz

--- Atari/assignment2/core/deep_q_learning.py
+++ Atari/assignment2/core/deep_q_learning.py
@@ -1,10 +1,454 @@
 import os
+import gym
 import numpy as np
-import tensorflow as tf
+import logging
 import time
+import sys
+from gym import wrappers
+from collections import deque
+import tensorflow as tf
 
-from q_learning import QN
+from utils.general import get_logger, Progbar, export_plot
+from utils.replay_buffer import ReplayBuffer
+from utils.preprocess import greyscale
+from utils.wrappers import PreproWrapper, MaxAndSkipEnv
 
+from utils.env import create_slither_env
+from universe.wrappers import Unvectorize
+
+class QN(object):
+    """
+    Abstract Class for implementing a Q Network
+    """
+    def __init__(self, env, config, logger=None):
+        """
+        Initialize Q Network and env
+
+        Args:
+            config: class with hyperparameters
+            logger: logger instance from logging module
+        """
+        # directory for training outputs
+        if not os.path.exists(config.output_path):
+            os.makedirs(config.output_path)
+
+        # store hyper params
+        self.config = config
+        self.logger = logger
+        if logger is None:
+            self.logger = get_logger(config.log_path)
+        self.env = env
+
+        # build model
+        self.build()
+
+
+    def build(self):
+        """
+        Build model
+        """
+        def build(self):
+        """
+        Build model by adding all necessary variables
+        """
+        # add placeholders
+        self.add_placeholders_op()
+
+        # compute Q values of state
+        s = self.process_state(self.s)
+        self.q = self.get_q_values_op(s, scope="q", reuse=False)
+
+        # compute Q values of next state
+        sp = self.process_state(self.sp)
+        self.target_q = self.get_q_values_op(sp, scope="target_q", reuse=False)
+
+        # add update operator for target network
+        self.add_update_target_op("q", "target_q")
+
+        # add square loss
+        self.add_loss_op(self.q, self.target_q)
+
+        # add optmizer for the main networks
+        self.add_optimizer_op("q")
+
+        total_parameters = 0
+        for variable in tf.trainable_variables():
+            shape = variable.get_shape()
+            variable_parametes = 1
+            for dim in shape:
+                variable_parametes *= dim.value
+            total_parameters += variable_parametes
+        print(total_parameters)
+
+
+    @property
+    def policy(self):
+        """
+        model.policy(state) = action
+        """
+        return lambda state: self.get_action(state)
+
+
+    def save(self):
+        """
+        Save model parameters
+
+        Args:
+            model_path: (string) directory
+        """
+        pass
+
+
+    def initialize(self):
+        """
+        Initialize variables if necessary
+        """
+        """
+        Assumes the graph has been constructed
+        Creates a tf Session and run initializer of variables
+        """
+        # create tf session
+        self.sess = tf.Session()
+
+        # tensorboard stuff
+        self.add_summary()
+
+        # initiliaze all variables
+        init = tf.global_variables_initializer()
+        self.sess.run(init)
+
+        # synchronise q and target_q networks
+        self.sess.run(self.update_target_op)
+
+        # for saving networks weights
+        self.saver = tf.train.Saver()
+
+    def add_summary(self):
+        """
+        Tensorboard stuff
+        """
+        # extra placeholders to log stuff from python
+        self.avg_reward_placeholder = tf.placeholder(tf.float32, shape=(), name="avg_reward")
+        self.max_reward_placeholder = tf.placeholder(tf.float32, shape=(), name="max_reward")
+        self.std_reward_placeholder = tf.placeholder(tf.float32, shape=(), name="std_reward")
+
+        self.avg_q_placeholder  = tf.placeholder(tf.float32, shape=(), name="avg_q")
+        self.max_q_placeholder  = tf.placeholder(tf.float32, shape=(), name="max_q")
+        self.std_q_placeholder  = tf.placeholder(tf.float32, shape=(), name="std_q")
+
+        self.eval_reward_placeholder = tf.placeholder(tf.float32, shape=(), name="eval_reward")
+
+        # add placeholders from the graph
+        tf.summary.scalar("loss", self.loss)
+        tf.summary.scalar("grads norm", self.grad_norm)
+
+        # extra summaries from python -> placeholders
+        tf.summary.scalar("Avg Reward", self.avg_reward_placeholder)
+        tf.summary.scalar("Max Reward", self.max_reward_placeholder)
+        tf.summary.scalar("Std Reward", self.std_reward_placeholder)
+
+        tf.summary.scalar("Avg Q", self.avg_q_placeholder)
+        tf.summary.scalar("Max Q", self.max_q_placeholder)
+        tf.summary.scalar("Std Q", self.std_q_placeholder)
+
+        tf.summary.scalar("Eval Reward", self.eval_reward_placeholder)
+
+        # logging
+        self.merged = tf.summary.merge_all()
+        self.file_writer = tf.summary.FileWriter(self.config.output_path,
+                                                self.sess.graph)
+
+
+    def get_best_action(self, state):
+        """
+        Returns best action according to the network
+
+        Args:
+            state: observation from gym
+        Returns:
+            tuple: action, q values
+        """
+        raise NotImplementedError
+
+
+    def get_action(self, state):
+        """
+        Returns action with some epsilon strategy
+
+        Args:
+            state: observation from gym
+        """
+        if np.random.random() < self.config.soft_epsilon:
+            return self.env.action_space.sample()
+        else:
+            return self.get_best_action(state)[0]
+
+
+    def update_target_params(self):
+        """
+        Update params of Q' with params of Q
+        """
+        raise NotImplementedError
+
+
+    def init_averages(self):
+        """
+        Defines extra attributes for tensorboard
+        """
+        self.avg_reward = -21.
+        self.max_reward = -21.
+        self.std_reward = 0
+
+        self.avg_q = 0
+        self.max_q = 0
+        self.std_q = 0
+
+        self.eval_reward = -21.
+
+
+    def update_averages(self, rewards, max_q_values, q_values, scores_eval):
+        """
+        Update the averages
+
+        Args:
+            rewards: deque
+            max_q_values: deque
+            q_values: deque
+            scores_eval: list
+        """
+        self.avg_reward = np.mean(rewards)
+        self.max_reward = np.max(rewards)
+        self.std_reward = np.sqrt(np.var(rewards) / len(rewards))
+
+        self.max_q      = np.mean(max_q_values)
+        self.avg_q      = np.mean(q_values)
+        self.std_q      = np.sqrt(np.var(q_values) / len(q_values))
+
+        if len(scores_eval) > 0:
+            self.eval_reward = scores_eval[-1]
+
+
+    def train(self, exp_schedule, lr_schedule):
+        """
+        Performs training of Q
+
+        Args:
+            exp_schedule: Exploration instance s.t.
+                exp_schedule.get_action(best_action) returns an action
+            lr_schedule: Schedule for learning rate
+        """
+
+        # initialize replay buffer and variables
+        replay_buffer = ReplayBuffer(self.config.buffer_size, self.config.state_history)
+        rewards = deque(maxlen=self.config.num_episodes_test)
+        max_q_values = deque(maxlen=1000)
+        q_values = deque(maxlen=1000)
+        self.init_averages()
+
+        t = last_eval = last_record = 0 # time control of nb of steps
+        scores_eval = [] # list of scores computed at iteration time
+        scores_eval += [self.evaluate()]
+
+        prog = Progbar(target=self.config.nsteps_train)
+
+        num_except = 0
+        # interact with environment
+        while t < self.config.nsteps_train:
+            try:
+                total_reward = 0
+                state = self.env.reset()
+                while True:
+                    t += 1
+                    last_eval += 1
+                    last_record += 1
+                    if self.config.render_train: self.env.render()
+                    # replay memory stuff
+                    idx      = replay_buffer.store_frame(state)
+                    q_input = replay_buffer.encode_recent_observation()
+
+                    # chose action according to current Q and exploration
+                    best_action, q_values = self.get_best_action(q_input)
+                    action                = exp_schedule.get_action(best_action)
+
+                    # store q values
+                    max_q_values.append(max(q_values))
+                    q_values += list(q_values)
+
+                    # perform action in env
+                    new_state, reward, done, info = self.env.step(action)
+
+                    # store the transition
+                    replay_buffer.store_effect(idx, action, reward, done)
+                    state = new_state
+
+                    # perform a training step
+                    loss_eval, grad_eval = self.train_step(t, replay_buffer, lr_schedule.epsilon)
+
+                    # logging stuff
+                    if ((t > self.config.learning_start) and (t % self.config.log_freq == 0) and
+                       (t % self.config.learning_freq == 0)):
+                        self.update_averages(rewards, max_q_values, q_values, scores_eval)
+                        exp_schedule.update(t)
+                        lr_schedule.update(t)
+                        if len(rewards) > 0:
+                            prog.update(t + 1, exact=[("Loss", loss_eval), ("Avg R", self.avg_reward),
+                                            ("Max R", np.max(rewards)), ("eps", exp_schedule.epsilon),
+                                            ("Grads", grad_eval), ("Max Q", self.max_q),
+                                            ("lr", lr_schedule.epsilon)])
+
+                    elif (t < self.config.learning_start) and (t % self.config.log_freq == 0):
+                        sys.stdout.write("\rPopulating the memory {}/{}...".format(t,
+                                                            self.config.learning_start))
+                        sys.stdout.flush()
+
+                    # count reward
+                    total_reward += reward
+                    if done or t >= self.config.nsteps_train:
+                        break
+
+                # updates to perform at the end of an episode
+                rewards.append(total_reward)
+
+                if (t > self.config.learning_start) and (last_eval > self.config.eval_freq):
+                    # evaluate our policy
+                    last_eval = 0
+                    print("")
+                    scores_eval += [self.evaluate()]
+
+                if (t > self.config.learning_start) and self.config.record and (last_record > self.config.record_freq):
+                    self.logger.info("Recording...")
+                    last_record =0
+                    self.record()
+
+            except Exception as e:
+                if num_except >= 25:
+                    break
+                time.sleep(60)
+                self.env = create_slither_env('shapes')
+                self.env = Unvectorize(self.env)
+                self.env.configure(fps=5.0, remotes=1, start_timeout=15 * 60, vnc_driver='go', vnc_kwargs={'encoding': 'tight', 'compress_level': 0, 'fine_quality_level': 50})
+        # last words
+        self.logger.info("- Training done.")
+        self.save()
+        scores_eval += [self.evaluate()]
+        export_plot(scores_eval, "Scores", self.config.plot_output)
+
+
+    def train_step(self, t, replay_buffer, lr):
+        """
+        Perform training step
+
+        Args:
+            t: (int) nths step
+            replay_buffer: buffer for sampling
+            lr: (float) learning rate
+        """
+        loss_eval, grad_eval = 0, 0
+
+        # perform training step
+        if (t > self.config.learning_start and t % self.config.learning_freq == 0):
+            loss_eval, grad_eval = self.update_step(t, replay_buffer, lr)
+
+        # occasionaly update target network with q network
+        if t % self.config.target_update_freq == 0:
+            self.update_target_params()
+
+        # occasionaly save the weights
+        if (t % self.config.saving_freq == 0):
+            self.save()
+
+        return loss_eval, grad_eval
+
+
+    def evaluate(self, env=None, num_episodes=None):
+        """
+        Evaluation with same procedure as the training
+        """
+        # log our activity only if default call
+        if num_episodes is None:
+            self.logger.info("Evaluating...")
+
+        # arguments defaults
+        if num_episodes is None:
+            num_episodes = self.config.num_episodes_test
+
+        if env is None:
+            env = self.env
+
+        # replay memory to play
+        replay_buffer = ReplayBuffer(self.config.buffer_size, self.config.state_history)
+        rewards = []
+
+        for i in range(num_episodes):
+            total_reward = 0
+            state = env.reset()
+            while True:
+                if self.config.render_test: env.render()
+
+                # store last state in buffer
+                idx     = replay_buffer.store_frame(state)
+                q_input = replay_buffer.encode_recent_observation()
+
+                action = self.get_action(q_input)
+
+                # perform action in env
+                new_state, reward, done, info = env.step(action)
+
+                # store in replay memory
+                replay_buffer.store_effect(idx, action, reward, done)
+                state = new_state
+
+                # count reward
+                total_reward += reward
+                if done:
+                    break
+
+            # updates to perform at the end of an episode
+            rewards.append(total_reward)
+
+        avg_reward = np.mean(rewards)
+        sigma_reward = np.sqrt(np.var(rewards) / len(rewards))
+
+        if num_episodes > 1:
+            msg = "Average reward: {:04.2f} +/- {:04.2f}".format(avg_reward, sigma_reward)
+            self.logger.info(msg)
+
+        return avg_reward
+
+
+    def record(self):
+        """
+        Re create an env and record a video for one episode
+        """
+        env = gym.make(self.config.env_name)
+        env = gym.wrappers.Monitor(env, self.config.record_path, video_callable=lambda x: True, resume=True)
+        env = MaxAndSkipEnv(env, skip=self.config.skip_frame)
+        env = PreproWrapper(env, prepro=greyscale, shape=(80, 80, 1),
+                        overwrite_render=self.config.overwrite_render)
+        self.evaluate(env, 1)
+
+
+    def run(self, exp_schedule, lr_schedule):
+        """
+        Apply procedures of training for a QN
+
+        Args:
+            exp_schedule: exploration strategy for epsilon
+            lr_schedule: schedule for learning rate
+        """
+        # initialize
+        self.initialize()
+
+        # record one game at the beginning
+        if self.config.record:
+            self.record()
+
+        # model
+        self.train(exp_schedule, lr_schedule)
+
+        # record one game at the end
+        if self.config.record:
+            self.record()
 
 class DQN(QN):
     """
@@ -23,9 +467,9 @@ class DQN(QN):
 
     def add_update_target_op(self, q_scope, target_q_scope):
         """
-        Update_target_op will be called periodically 
+        Update_target_op will be called periodically
         to copy Q network to target Q network
-    
+
         Args:
             q_scope: name of the scope of variables for q
             target_q_scope: name of the scope of variables for the target
@@ -121,7 +565,7 @@ class DQN(QN):
         # for saving networks weights
         self.saver = tf.train.Saver()
 
-       
+
     def add_summary(self):
         """
         Tensorboard stuff
@@ -151,10 +595,10 @@ class DQN(QN):
         tf.summary.scalar("Std Q", self.std_q_placeholder)
 
         tf.summary.scalar("Eval Reward", self.eval_reward_placeholder)
-            
+
         # logging
         self.merged = tf.summary.merge_all()
-        self.file_writer = tf.summary.FileWriter(self.config.output_path, 
+        self.file_writer = tf.summary.FileWriter(self.config.output_path,
                                                 self.sess.graph)
 
 
@@ -204,26 +648,26 @@ class DQN(QN):
             self.s: s_batch,
             self.a: a_batch,
             self.r: r_batch,
-            self.sp: sp_batch, 
+            self.sp: sp_batch,
             self.done_mask: done_mask_batch,
-            self.lr: lr, 
+            self.lr: lr,
             # extra info
-            self.avg_reward_placeholder: self.avg_reward, 
-            self.max_reward_placeholder: self.max_reward, 
-            self.std_reward_placeholder: self.std_reward, 
-            self.avg_q_placeholder: self.avg_q, 
-            self.max_q_placeholder: self.max_q, 
-            self.std_q_placeholder: self.std_q, 
-            self.eval_reward_placeholder: self.eval_reward, 
+            self.avg_reward_placeholder: self.avg_reward,
+            self.max_reward_placeholder: self.max_reward,
+            self.std_reward_placeholder: self.std_reward,
+            self.avg_q_placeholder: self.avg_q,
+            self.max_q_placeholder: self.max_q,
+            self.std_q_placeholder: self.std_q,
+            self.eval_reward_placeholder: self.eval_reward,
         }
 
-        loss_eval, grad_norm_eval, summary, _ = self.sess.run([self.loss, self.grad_norm, 
+        loss_eval, grad_norm_eval, summary, _ = self.sess.run([self.loss, self.grad_norm,
                                                  self.merged, self.train_op], feed_dict=fd)
 
 
         # tensorboard stuff
         self.file_writer.add_summary(summary, t)
-        
+
         return loss_eval, grad_norm_eval
 
 
